0f7b177fbbfb279bd224a1bd56d067e4a3be9779
s7imKa <maksikmaksik2005@gmail.com>
2025-08-22 14:49:59 +0300

    Refactor CharListItem component to use class syntax; enhance keyboard accessibility and semantic roles for better usability

--- src/components/charListItem/CharListItem.jsx
+++ src/components/charListItem/CharListItem.jsx
@@ -1,20 +1,44 @@
+import { Component } from 'react'
+
 import PropTypes from 'prop-types'
 
 import './charListItem.css'
 
-const CharListItem = ({ id, name, thumbnail, onSelectedChar, selectedChar }) => {
-    const classActive = selectedChar === id ? 'char-item char-item-active' : 'char-item'
-    return (
-        <li className={classActive} onClick={() => onSelectedChar(id)}>
-            <img src={thumbnail} alt={name} />
-            <div className='char__name'>{name}</div>
-        </li>
-    )
+class CharListItem extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    handleKeyDown = (e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault()
+            this.props.onSelectedChar(this.props.id)
+        }
+    }
+    render() {
+        const { id, name, thumbnail, onSelectedChar, selectedChar } = this.props
+        const classActive =
+            selectedChar === id ? 'char-item char-item-active' : 'char-item'
+
+        return (
+            <li
+                className={classActive}
+                onClick={() => onSelectedChar(id)}
+                onKeyDown={this.handleKeyDown} // ✅ підтримка клавіатури
+                tabIndex={0} // ✅ можна фокусувати через Tab
+                role='button' // ✅ семантична роль
+                aria-label={`Select character ${name}`} // ✅ доступність
+            >
+                <img src={thumbnail} alt={name} />
+                <div className='char__name'>{name}</div>
+            </li>
+        )
+    }
 }
 
 CharListItem.PropTypes = {
     onSelectedChar: PropTypes.func.isRequired,
-    selectedChar: PropTypes.number
+    selectedChar: PropTypes.number,
 }
 
 export default CharListItem
