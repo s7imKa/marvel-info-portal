a56300ee4ffd39c88d9e65001dc2c05c7ab05644
s7imKa <maksikmaksik2005@gmail.com>
2025-08-26 00:38:23 +0300

    Refactor RandomChar component: streamline imports and improve code consistency

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,121 +1,122 @@
-import { Component } from 'react';
+import { Component } from 'react'
 
-import mjolnir from '../../assets/images/img-char-random-section/mjolnir.png';
-import shield from '../../assets/images/img-char-random-section/shield.png';
+import mjolnir from '../../assets/images/img-char-random-section/mjolnir.png'
+import shield from '../../assets/images/img-char-random-section/shield.png'
 
-import MarvelService from '../../services/MarvelService';
-import Error from '../error/Error';
-import Loader from '../loader/Loader';
+import MarvelService from '../../services/MarvelService'
+import Error from '../error/Error'
+import Loader from '../loader/Loader'
 
-import '../../assets/styles/button.css';
-import './randomChar.css';
+import '../../assets/styles/button.css'
+import './randomChar.css'
 
 class RandomChar extends Component {
-   constructor(props) {
-      super(props);
-   }
+    constructor(props) {
+        super(props)
+    }
 
-   state = {
-      char: {}, // дані персонажа
-      loader: true, // стан завантаження
-      error: false,
-   };
+    state = {
+        char: {}, // дані персонажа
+        loader: true, // стан завантаження
+        error: false,
+    }
 
-   marvelService = new MarvelService(); // сервіс Marvel API
+    marvelService = new MarvelService() // сервіс Marvel API
 
-   componentDidMount() {
-      this.updataChar();
-   }
+    componentDidMount() {
+        this.updataChar()
+    }
 
-   onCharLoaded = (char) => {
-      this.setState({
-         char: char, // зберігаємо персонажа
-         loader: false, // вимикаємо loader
-         error: false, // ВАЖЛИВО: скидаємо помилку при успішному завантаженні
-      });
-   };
+    onCharLoaded = (char) => {
+        this.setState({
+            char: char, // зберігаємо персонажа
+            loader: false, // вимикаємо loader
+            error: false, // ВАЖЛИВО: скидаємо помилку при успішному завантаженні
+        })
+    }
 
-   onError = () => {
-      this.setState({
-         error: true,
-         loader: false,
-      });
-   };
+    onError = () => {
+        this.setState({
+            error: true,
+            loader: false,
+        })
+    }
 
-   updataChar = () => {
-      const max = 22;
-      const id = Math.floor(Math.random() * max) + 1; // випадковий id
+    updataChar = () => {
+        const max = 22
+        const id = Math.floor(Math.random() * max) + 1 // випадковий id
 
-      // Скидаємо error стан при новому запиті
-      this.setState({
-         loader: true,
-         error: false,
-      });
+        // Скидаємо error стан при новому запиті
+        this.setState({
+            loader: true,
+            error: false,
+        })
 
-      this.marvelService
-         .getCharacters(id)
-         .then((res) => this.onCharLoaded(res))
-         .catch(this.onError);
-   };
+        this.marvelService
+            .getCharacters(id)
+            .then((res) => this.onCharLoaded(res))
+            .catch(this.onError)
+    }
 
-   render() {
-      const { char, loader, error } = this.state;
-      const errorView = error ? <Error /> : null;
-      const loaderView = loader ? <Loader /> : null;
-      const contentView = !(loader || error) ? <Char char={char} /> : null;
+    render() {
+        const { char, loader, error } = this.state
+        const errorView = error ? <Error /> : null
+        const loaderView = loader ? <Loader /> : null
+        const contentView = !(loader || error) ? <Char char={char} /> : null
 
-      return (
-         <section className='random-char-section'>
-            <div className='char-info'>
-               {errorView}
-               {loaderView}
-               {contentView}
-            </div>
-            <div className='char-random'>
-               <img className='mjolnir' src={mjolnir} alt='mjolnir' /> {/* картинка */}
-               <img className='shield' src={shield} alt='shield' /> {/* картинка */}
-               <div className='char-random-content'>
-                  <h1>
-                     Random character for today! <br />
-                     Do you want to get to know him better?
-                  </h1>
-                  <div className='button-random-car'>
-                     <h1>Or choose another one</h1>
-                     <button
-                        className='button'
-                        onClick={this.updataChar}
-                        disabled={loader}
-                     >
-                        {loader ? 'LOADING...' : 'TRY IT'}
-                     </button>
-                  </div>
-               </div>
-            </div>
-         </section>
-      );
-   }
+        return (
+            <section className='random-char-section'>
+                <div className='char-info'>
+                    {errorView}
+                    {loaderView}
+                    {contentView}
+                </div>
+                <div className='char-random'>
+                    <img className='mjolnir' src={mjolnir} alt='mjolnir' />{' '}
+                    {/* картинка */}
+                    <img className='shield' src={shield} alt='shield' /> {/* картинка */}
+                    <div className='char-random-content'>
+                        <h1>
+                            Random character for today! <br />
+                            Do you want to get to know him better?
+                        </h1>
+                        <div className='button-random-car'>
+                            <h1>Or choose another one</h1>
+                            <button
+                                className='button'
+                                onClick={this.updataChar}
+                                disabled={loader}
+                            >
+                                {loader ? 'LOADING...' : 'TRY IT'}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 const Char = ({ char }) => {
-   const { name, description, thumbnail, homepage, wiki } = char; // деструктуризація
+    const { name, description, thumbnail, homepage, wiki } = char // деструктуризація
 
-   return (
-      <>
-         <img className='thumbnail' src={thumbnail} alt='img-char' /> {/* фото */}
-         <div className='char-info-content'>
-            <h2>{name}</h2> {/* ім'я */}
-            <p>{description}</p> {/* опис */}
-            <div className='button-char-info'>
-               <a href={homepage} className='button'>
-                  <div className='inner'>HOMEPAGE</div>
-               </a>
-               <a href={wiki} className='button button-darck'>
-                  <div className='inner'> WIKI</div>
-               </a>
+    return (
+        <>
+            <img className='thumbnail' src={thumbnail} alt='img-char' /> {/* фото */}
+            <div className='char-info-content'>
+                <h2>{name}</h2> {/* ім'я */}
+                <p>{description}</p> {/* опис */}
+                <div className='button-char-info'>
+                    <a href={homepage} className='button'>
+                        <div className='inner'>HOMEPAGE</div>
+                    </a>
+                    <a href={wiki} className='button button-darck'>
+                        <div className='inner'> WIKI</div>
+                    </a>
+                </div>
             </div>
-         </div>
-      </>
-   );
-};
+        </>
+    )
+}
 
-export default RandomChar;
+export default RandomChar
