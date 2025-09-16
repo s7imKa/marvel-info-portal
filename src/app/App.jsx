11123c7804ec15ef7ad3a2ff39f4c0df75d1bfc3
s7imKa <maksikmaksik2005@gmail.com>
2025-09-16 18:02:53 +0300

    refactor: clean up imports and formatting in App and FormCharInfo components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,11 @@
-import { AnimatePresence } from 'framer-motion'
-import React, { Suspense } from 'react'
-import { Helmet } from 'react-helmet'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import {AnimatePresence} from 'framer-motion'
+import React, {Suspense} from 'react'
+import {Helmet} from 'react-helmet'
+import {Route, Routes, useLocation} from 'react-router-dom'
 
 import InfoCharCard from '../components/InfoCharCard/InfoCharCard'
 import InfoComicsCard from '../components/InfoComicsCard/InfoComicsCard'
-import { AppBanner } from '../components/layout/appBanner/AppBanner'
+import {AppBanner} from '../components/layout/appBanner/AppBanner'
 import AppHeader from '../components/layout/appHeader/AppHeader'
 import Loader from '../components/loader/Loader'
 import NotFoundPage from '../components/NotFoundPage/NotFoundPage'
@@ -25,69 +25,69 @@ const App = () => {
 
     return (
         <AnimatePresence mode='wait'>
-                <div className='app'>
-                    <AppHeader />
-                    <main>
-                        <Routes location={location} key={location.pathname}>
-                            <Route
-                                path='/'
-                                element={
-                                    <>
-                                        <Suspense fallback={<Loader />}>
-                                            <Helmet>
-                                                <meta
-                                                    name='description'
-                                                    content='Marvel information portal'
-                                                />
-                                                <title>Marvel portal</title>
-                                            </Helmet>
-                                            <RandomChar />
-                                            <SectionCharPanel />
-                                        </Suspense>
-                                    </>
-                                }
-                            />
-                            <Route
-                                path='/char/:id'
-                                element={
-                                    <>
-                                        <Suspense fallback={<Loader />}>
-                                            <InfoCharCard />
-                                        </Suspense>
-                                    </>
-                                }
-                            />
-
-                            <Route
-                                path='/comics'
-                                element={
-                                    <Suspense fallback={<Loader />}>
+            <div className='app'>
+                <AppHeader/>
+                <main>
+                    <Routes location={location} key={location.pathname}>
+                        <Route
+                            path='/'
+                            element={
+                                <>
+                                    <Suspense fallback={<Loader/>}>
                                         <Helmet>
                                             <meta
                                                 name='description'
-                                                content='Page with list of our comics'
+                                                content='Marvel information portal'
                                             />
-                                            <title>Comics</title>
+                                            <title>Marvel portal</title>
                                         </Helmet>
-                                        <SectionComicsPanel />
+                                        <RandomChar/>
+                                        <SectionCharPanel/>
                                     </Suspense>
-                                }
-                            />
-                            <Route
-                                path='/comics/:id'
-                                element={
-                                    <>
-                                        <AppBanner />
-                                        <Suspense fallback={<Loader />}>
-                                            <InfoComicsCard />
-                                        </Suspense>
-                                    </>
-                                }
-                            />
-                            <Route path='*' element={<NotFoundPage />} />
-                        </Routes>
-                    </main>
-                </div>
+                                </>
+                            }
+                        />
+                        <Route
+                            path='/char/:id'
+                            element={
+                                <>
+                                    <Suspense fallback={<Loader/>}>
+                                        <InfoCharCard/>
+                                    </Suspense>
+                                </>
+                            }
+                        />
+
+                        <Route
+                            path='/comics'
+                            element={
+                                <Suspense fallback={<Loader/>}>
+                                    <Helmet>
+                                        <meta
+                                            name='description'
+                                            content='Page with list of our comics'
+                                        />
+                                        <title>Comics</title>
+                                    </Helmet>
+                                    <SectionComicsPanel/>
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path='/comics/:id'
+                            element={
+                                <>
+                                    <AppBanner/>
+                                    <Suspense fallback={<Loader/>}>
+                                        <InfoComicsCard/>
+                                    </Suspense>
+                                </>
+                            }
+                        />
+                        <Route path='*' element={<NotFoundPage/>}/>
+                    </Routes>
+                </main>
+            </div>
         </AnimatePresence>
     )
 }
--- src/components/formCharInfo/FormCharInfo.jsx
+++ src/components/formCharInfo/FormCharInfo.jsx
@@ -1,66 +1,65 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik'
-import { useState } from 'react'
-import * as Yup from 'yup'
-import { useNavigate } from 'react-router-dom'
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import {useState} from "react";
+import * as Yup from "yup";
+import {useNavigate} from "react-router-dom";
 
-import useMarvelService from '../../services/MarvelService'
+import useMarvelService from "../../services/MarvelService";
 
-import './formCharInfo.css'
-import '../../assets/styles/button.css'
+import "./formCharInfo.css";
+import "../../assets/styles/button.css";
 
 const FormCharInfo = () => {
-    const navigate = useNavigate()
-    const [resultFindChar, setResultFindChar] = useState([])
+    const navigate = useNavigate();
+    const [resultFindChar, setResultFindChar] = useState([]);
 
-    const { getAllCharacters } = useMarvelService()
+    const {getAllCharacters} = useMarvelService();
 
-    const searchChar = async value => {
-        const allChars = await getAllCharacters(0, 20)
+    const searchChar = async (value) => {
+        const allChars = await getAllCharacters(0, 20);
         const res = allChars.filter(
-            item => item.name.toLowerCase() === value.search.toLowerCase(),
-        )
-        setResultFindChar(res[0])
-         if (res[0]) {
-             navigate(`/char/${res[0].id}`)
-         }
-        
-    }
+            (item) => item.name.toLowerCase() === value.search.toLowerCase(),
+        );
+        setResultFindChar(res[0]);
+        if (res[0]) {
+            navigate(`/char/${res[0].id}`);
+        }
+    };
 
-    console.log(resultFindChar)
+    console.log(resultFindChar);
 
     return (
         <Formik
             initialValues={{
-                search: '',
+                search: "",
             }}
             validationSchema={Yup.object({
                 search: Yup.string()
-                    .min(2, 'Мінімальна кількість символів 2!')
-                    .matches(/^[^\d]+$/, 'Тільки букви, без цифр!')
-                    .required('Обовязкове поле...'),
+                    .min(2, "Мінімальна кількість символів 2!")
+                    .matches(/^[^\d]+$/, "Тільки букви, без цифр!")
+                    .required("Обовязкове поле..."),
             })}
-            onSubmit={values => searchChar(values)}
+            onSubmit={(values) => searchChar(values)}
         >
-            {({ errors, isSubmitting }) => (
-                <Form className='form-serach'>
-                    <label htmlFor='text'>Or find a character by name:</label>
+            {({errors, isSubmitting}) => (
+                <Form className="form-serach">
+                    <label htmlFor="text">Or find a character by name:</label>
                     <Field
-                        id='search'
-                        name='search'
-                        type='text'
-                        placeholder={'Enter name...'}
+                        id="search"
+                        name="search"
+                        type="text"
+                        placeholder={"Enter name..."}
                     />
                     {!resultFindChar && !errors.search ? (
                         <div>Героя не знайдено !</div>
                     ) : null}
-                    <ErrorMessage name='search' component={'div'} />
-                    <button className={'button'}type='submit' disabled={isSubmitting}>
+                    <ErrorMessage name="search" component={"div"}/>
+                    <button className={"button"} type="submit" disabled={isSubmitting}>
                         Пошук
                     </button>
                 </Form>
             )}
         </Formik>
-    )
-}
+    );
+};
 
-export default FormCharInfo
+export default FormCharInfo;
